0a3d86bbdc206c111943f9cd1eadfe9ba2af200b
Jan Kofron <jan.kofron@d3s.mff.cuni.cz>
2024-11-21 16:20:58 +0100

    Updating scripts to accept .i files and generate v1 witnesses as witness.graphml in current folder

--- bin/run-chc.py
+++ bin/run-chc.py
@@ -122,6 +122,7 @@ def create_witness_v1(witness_type):
     # data['content'] = ''
 
     output_file = Path(input_file).stem + '.graphml'
+    # output_file =  Path(input_file).parent.joinpath('witness.graphml')
     uuid = uuid.uuid4()
     with open(input_cfile, 'rb') as f:
         sha256 = hashlib.sha256(f.read()).hexdigest()
