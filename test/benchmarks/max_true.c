5ae3ec17a1b7fd69a6b938f3d5be26f1f08ce3de
Martin Blicha <martin.blicha@gmail.com>
2025-04-15 12:35:49 +0200

    Add very basic test infrastructure

---
+++ test/benchmarks/max_true.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int getInt();
+
+int main() {
+    int a = getInt();
+    int b = getInt();
+    int m = max(a,b);
+    assert(m >= a && m >= b);
+}
---
+++ test/run_tests.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+test_dir="${script_dir}/benchmarks"
+executable="${script_dir}/../build/src/hornix"
+
+if ! command -v clang &> /dev/null; then
+  echo "Error: clang is not installed or not in PATH."
+  exit 1
+fi
+
+CLANG_VERSION=$(clang --version | head -n1 | sed -E 's/.*clang version ([0-9]+).*/\1/')
+
+if [[ "$CLANG_VERSION" -lt 18 ]]; then
+  echo "Error: clang version must be 18 or higher. Found: $CLANG_VERSION"
+  exit 1
+fi
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Iterate over each .c file in the directory
+for bench in "${test_dir}"/*.c; do
+  [[ -e "${bench}" ]] || continue  # Skip if no .c files
+
+  BASENAME=$(basename "${bench}" .c)
+
+  # Check if filename ends with "true" or "false"
+  if [[ "$BASENAME" =~ (true|false)$ ]]; then
+    EXPECTED="${BASH_REMATCH[1]}"
+    
+    IR_FILE="${BASENAME}.ll"
+    # First compile using clang to check for errors
+    if ! clang -Xclang=-disable-O0-optnone -S -emit-llvm "${bench}" -o ${IR_FILE}  &> /dev/null; then
+      echo -e "${BASENAME}\t${READ}FAIL{$NC} (clang compile error)"
+      ((fail_count++))
+      continue
+    fi
+
+    OUTPUT="$(${executable} ${IR_FILE})"
+    #echo ${OUTPUT}
+    
+    rm ${IR_FILE}
+
+    # Get the first word from the output
+    FIRST_WORD=$(echo "$OUTPUT" | awk '{print tolower($1)}')
+    if [[ "$FIRST_WORD" == "sat" ]]; then
+        FIRST_WORD="true"
+    fi
+    if [[ "$FIRST_WORD" == "unsat" ]]; then
+        FIRST_WORD="false"
+    fi
+
+    if [[ "$FIRST_WORD" == "true" || "$FIRST_WORD" == "false" ]]; then
+      if [[ "$FIRST_WORD" == "$EXPECTED" ]]; then
+        echo -e "${BASENAME}\t${GREEN}PASS${NC}"
+        ((pass_count++))
+      else
+        echo -e "${BASENAME}\t${RED}FAIL${NC}"
+        ((fail_count++))
+      fi
+    else
+      echo -e "${BASENAME}\t${RED}FAIL${NC}"
+      ((fail_count++))
+    fi
+  else
+    echo "Skipping: ${BASENAME} (filename doesn't end with 'true' or 'false')"
+    ((skip_count++))
+  fi
+done
+
+echo
+echo "========== Summary =========="
+echo -e "✅ Passed: ${GREEN}${pass_count}${NC}"
+echo -e "❌ Failed: ${RED}${fail_count}${NC}"
+echo -e "⏭️  Skipped: $skip_count"
+
+
+# Exit with non-zero status if any test failed
+if [[ $fail_count -gt 0 ]]; then
+  exit 1
+else
+  exit 0
+fi
