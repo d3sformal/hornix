b70493d3af61e599d1e0eb30a4ae3d2eff8dab50
Martin Blicha <martin.blicha@gmail.com>
2025-04-17 09:21:29 +0200

    Update script for running SV-COMP benchmarks

--- scripts/test-set.sh
+++ scripts/test-set.sh
@@ -8,11 +8,14 @@ fi
 file="$1"
 timeout="$2"
 
+file=$(realpath ${file})
+svcomp_dir=$(dirname ${file})
+
 while read line; do
   if [[ "$line" =~ "#" ]]; then 
    continue 1
   else
    repo=$(echo "${line%%/*}")
-   ./test-bench.sh "sv-benchmarks/c/$repo/" $timeout
+   ./test-bench.sh "${svcomp_dir}/$repo/" $timeout
   fi
 done < $file
