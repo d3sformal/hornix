09976cdf0de5eda78ae1a297172d056b2b9b6645
Martin Blicha <martin.blicha@gmail.com>
2025-05-01 22:19:18 +0200

    Simplify script for running tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,18 +4,6 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 test_dir="${script_dir}/benchmarks"
 executable="${script_dir}/../build/src/hornix"
 
-if ! command -v clang &> /dev/null; then
-  echo "Error: clang is not installed or not in PATH."
-  exit 1
-fi
-
-CLANG_VERSION=$(clang --version | head -n1 | sed -E 's/.*clang version ([0-9]+).*/\1/')
-
-if [[ "$CLANG_VERSION" -lt 18 ]]; then
-  echo "Error: clang version must be 18 or higher. Found: $CLANG_VERSION"
-  exit 1
-fi
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -30,20 +18,9 @@ for bench in "${test_dir}"/*.c; do
   if [[ "$BASENAME" =~ (true|false)$ ]]; then
     EXPECTED="${BASH_REMATCH[1]}"
     
-    IR_FILE="${BASENAME}.ll"
-    # First compile using clang to check for errors
-    if ! clang -Xclang=-disable-O0-optnone -S -emit-llvm "${bench}" -o ${IR_FILE}  &> /dev/null; then
-      printf "%-30s ${RED}FAIL${NC}" "${BASENAME}"
-      echo " (clang compile error)"
-      ((fail_count++))
-      continue
-    fi
-
-    OUTPUT="$(${executable} ${IR_FILE})"
+    OUTPUT="$(${executable} ${bench})"
     #echo ${OUTPUT}
     
-    rm ${IR_FILE}
-
     # Get the first word from the output
     FIRST_WORD=$(echo "$OUTPUT" | awk '{print tolower($1)}')
     if [[ "$FIRST_WORD" == "sat" ]]; then
