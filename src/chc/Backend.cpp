6a23eba28e52a9c730b482dc0daa39a08a2d2f68
Martin Blicha <martin.blicha@gmail.com>
2025-05-13 14:57:51 +0200

    Replace usage of std::tmpnam
    Compilers warn against usage of this function. Instead we use LLVM's
    ability to locate temporary directory and we create out query file
    there.

--- src/chc/Backend.cpp
+++ src/chc/Backend.cpp
@@ -41,9 +41,13 @@ Result solve(std::string query) {
 
 
 Result solve(std::string query, SolverContext context) {
+    std::error_code ec;
+    auto tmp_dir = fs::temp_directory_path(ec);
+    if (tmp_dir.empty()) {
+        throw std::logic_error("Could not locate temporary directory");
+    }
+    auto const smtfile = tmp_dir / "hornix_query.smt2";
     // Write the query to a temporary file
-    std::string smtfile = std::tmpnam(nullptr);
-    smtfile += ".smt2";
     std::ofstream tempFile(smtfile);
     if (!tempFile) {
         throw std::logic_error("Could not create a temporary file");
@@ -52,17 +56,20 @@ Result solve(std::string query, SolverContext context) {
     tempFile << query;
     tempFile.close();
 
-    std::string response_path = smtfile + ".out";
+    fs::path response_path = smtfile;
+    response_path.replace_extension("out");
     std::optional<std::string> solver_dir = context.solver_dir.has_value() ? context.solver_dir.value() + fs::path::preferred_separator : std::optional<std::string>{};
-    std::string command = solver_dir.value_or("") + context.solver + " " + context.args + " " + smtfile + " >" + response_path;
+    std::string command = solver_dir.value_or("") + context.solver + " " + context.args + " " + smtfile.string() + " >" + response_path.string();
     std::system(command.c_str());
 
-    std::remove(smtfile.c_str());
+    fs::remove(smtfile, ec);
 
     std::ifstream file(response_path); // opens in text mode by default
     std::stringstream buffer;
     buffer << file.rdbuf(); // read entire file into buffer
     std::string response = buffer.str();
+    file.close();
+    fs::remove(response_path);
     return response;
 }
 } // namespace hornix
