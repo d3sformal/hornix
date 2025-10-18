2fdfa83d6ebb8059547a55d41e89174c50223b11
Martin Blicha <martin.blicha@gmail.com>
2025-10-18 10:51:27 +0200

    Reformat file

--- src/chc/SMTOut.hpp
+++ src/chc/SMTOut.hpp
@@ -15,33 +15,33 @@
 namespace hornix {
 class SMTOutput {
 public:
-  SMTOutput() : output(std::cout) {}
-  explicit SMTOutput(std::ostream & out) : output(out) {}
+    SMTOutput() : output(std::cout) {}
+    explicit SMTOutput(std::ostream & out) : output(out) {}
 
-  void smt_print_implications(std::vector<Implication> const & implications);
+    void smt_print_implications(std::vector<Implication> const & implications);
 
 private:
-  void smt_declare_functions(std::vector<Implication> const & implications);
+    void smt_declare_functions(std::vector<Implication> const & implications);
 
-  void smt_declare_function(MyConstraint const & predicate);
+    void smt_declare_function(MyConstraint const & predicate);
 
-  void smt_print_constraints(Implication::Constraints const & constraints);
+    void smt_print_constraints(Implication::Constraints const & constraints);
 
-  void smt_declare_implication(Implication const & implication);
+    void smt_declare_implication(Implication const & implication);
 
-  void smt_declare_implications(std::vector<Implication> const & implications) {
-    for (auto const & implication : implications) {
-      smt_declare_implication(implication);
+    void smt_declare_implications(std::vector<Implication> const & implications) {
+        for (auto const & implication : implications) {
+            smt_declare_implication(implication);
+        }
     }
-  }
 
-  int smt_quantifiers(Implication const & implication, int indent);
+    int smt_quantifiers(Implication const & implication, int indent);
 
-  void print_header() const;
-  void print_footer() const;
+    void print_header() const;
+    void print_footer() const;
 
-  std::ostream & output;
-  std::unordered_set<std::string> declared_functions;
+    std::ostream & output;
+    std::unordered_set<std::string> declared_functions;
 };
 } // namespace hornix
 
